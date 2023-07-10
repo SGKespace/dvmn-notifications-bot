5aede6c05c13aa3fedad3603996dfd9ca73613c9
SGK <55636018+SGKespace@users.noreply.github.com>
2023-07-10 20:07:46 +0400

    send logs to Telegram to admin user.

--- bot.py
+++ bot.py
@@ -3,13 +3,28 @@ import os
 from time import time, sleep
 import telegram
 from dotenv import load_dotenv
+import logging
 
 
-def start(dvmn_token, chat_id, bot):
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
+
+def start(dvmn_token, chat_id, tg_bot):
     long_polling_url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': dvmn_token}
     params = {'timestamp': time()}
+
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(tg_bot, chat_id))
+    logger.info('Бот в деле')
 
     while True:
         try:
@@ -25,17 +40,19 @@ def start(dvmn_token, chat_id, bot):
                 work_link = checks["new_attempts"][0]["lesson_url"]
                 work_status = checks["new_attempts"][0]["is_negative"]
                 if work_status:
-                    bot.send_message(chat_id=chat_id, text=f"Урок '{work_title}' не прошел ревью. [Что от тебя хотят в этот раз]({work_link})", parse_mode='Markdown', disable_web_page_preview=True)
+                    tg_bot.send_message(chat_id=chat_id, text=f"Урок '{work_title}' не прошел ревью. [Что от тебя хотят в этот раз]({work_link})", parse_mode='Markdown', disable_web_page_preview=True)
                 elif not work_status:
-                    bot.send_message(chat_id=chat_id, text=f"По уроку '{work_title}' зачет получен.\n{work_link}", parse_mode="html", disable_web_page_preview=True)
+                    tg_bot.send_message(chat_id=chat_id, text=f"По уроку '{work_title}' зачет получен.\n{work_link}", parse_mode="html", disable_web_page_preview=True)
                 params = {'timestamp': last_attempt_timestamp}
-        except requests.exceptions.Timeout:
+        except requests.exceptions.Timeout as error:
+            logger.critical(f'Error:  {error}')
             continue
-        except (requests.exceptions.HTTPError, requests.RequestException):
+        except (requests.exceptions.HTTPError, requests.RequestException) as error:
+            logger.critical(f'Error:  {error}')
             sleep(90)
-        except requests.ConnectionError:
-            print('Нет связи, повторная попытка через 10 сек.')
-            sleep(10)
+        except requests.ConnectionError as error:
+            logger.critical(f'Error:  {error}')
+            sleep(90)
 
 
 def main():
@@ -43,8 +60,9 @@ def main():
     telegram_token = os.environ["TELEGRAM_TOKEN"]
     dvmn_token = os.environ["DVMN_TOKEN"]
     chat_id = int(os.environ['TG_CHAT_ID'])
-    bot = telegram.Bot(token=telegram_token)
-    start(dvmn_token, chat_id, bot)
+    tg_bot = telegram.Bot(token=telegram_token)
+
+    start(dvmn_token, chat_id, tg_bot)
 
 
 if __name__ == "__main__":
