c10a9128a6474b4962e3ad77540a7576fa523e64
SGK <55636018+SGKespace@users.noreply.github.com>
2023-07-11 15:08:34 +0400

    Работа над ошибками

--- bot.py
+++ bot.py
@@ -5,23 +5,31 @@ import telegram
 from dotenv import load_dotenv
 import logging
 
+logger = logging.getLogger('Logger')
 
 class TelegramLogsHandler(logging.Handler):
+
     def __init__(self, tg_bot, chat_id):
         super().__init__()
         self.chat_id = chat_id
         self.tg_bot = tg_bot
+
     def emit(self, record):
         log_entry = self.format(record)
         self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
-def start(dvmn_token, chat_id, tg_bot):
+def main():
+    load_dotenv()
+    telegram_token = os.environ["TELEGRAM_TOKEN"]
+    dvmn_token = os.environ["DVMN_TOKEN"]
+    chat_id = int(os.environ['TG_CHAT_ID'])
+    tg_bot = telegram.Bot(token=telegram_token)
+
     long_polling_url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': dvmn_token}
     params = {'timestamp': time()}
 
-    logger = logging.getLogger('Logger')
     logger.setLevel(logging.INFO)
     logger.addHandler(TelegramLogsHandler(tg_bot, chat_id))
     logger.info('Бот в деле')
@@ -40,29 +48,21 @@ def start(dvmn_token, chat_id, tg_bot):
                 work_link = checks["new_attempts"][0]["lesson_url"]
                 work_status = checks["new_attempts"][0]["is_negative"]
                 if work_status:
-                    tg_bot.send_message(chat_id=chat_id, text=f"Урок '{work_title}' не прошел ревью. [Что от тебя хотят в этот раз]({work_link})", parse_mode='Markdown', disable_web_page_preview=True)
+                    tg_bot.send_message(chat_id=chat_id,
+                                        text=f"Урок '{work_title}' не прошел ревью. [Что от тебя хотят в этот раз]({work_link})",
+                                        parse_mode='Markdown', disable_web_page_preview=True)
                 elif not work_status:
-                    tg_bot.send_message(chat_id=chat_id, text=f"По уроку '{work_title}' зачет получен.\n{work_link}", parse_mode="html", disable_web_page_preview=True)
+                    tg_bot.send_message(chat_id=chat_id, text=f"По уроку '{work_title}' зачет получен.\n{work_link}",
+                                        parse_mode="html", disable_web_page_preview=True)
                 params = {'timestamp': last_attempt_timestamp}
         except requests.exceptions.Timeout as error:
-            logger.critical(f'Error:  {error}')
             continue
         except (requests.exceptions.HTTPError, requests.RequestException) as error:
             logger.critical(f'Error:  {error}')
-            sleep(90)
+            sleep(60)
         except requests.ConnectionError as error:
             logger.critical(f'Error:  {error}')
-            sleep(90)
-
-
-def main():
-    load_dotenv()
-    telegram_token = os.environ["TELEGRAM_TOKEN"]
-    dvmn_token = os.environ["DVMN_TOKEN"]
-    chat_id = int(os.environ['TG_CHAT_ID'])
-    tg_bot = telegram.Bot(token=telegram_token)
-
-    start(dvmn_token, chat_id, tg_bot)
+            sleep(60)
 
 
 if __name__ == "__main__":
